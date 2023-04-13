79aabb4c95a5383e276330aba375bd121d30da80
Ángel Lareo <angel.lareo@gmail.com>
2023-04-14 00:22:22 +0200

    Updates to v1.2

--- Software/clock/clock.ino
+++ Software/clock/clock.ino
@@ -1,29 +1,53 @@
 // RTC.configure(MOSI, MISO, CLK, CS)
 // RTC.setDateTime(day, month, year, hour, minute, second)
  
-#include <DS3234.h>
- 
+#include <RtcDS3234.h>
+#include <SPI.h>
+
+const uint8_t DS3234_CS_PIN = 10; // RTC SS pin
+
+RtcDS3234<SPIClass> RTC(SPI, DS3234_CS_PIN);
+
 void setup()
 {
-  Serial.begin(19200);
-  
-  //Pin configurations for all OSBSS dataloggers:
-  pinMode(6, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(3, OUTPUT);
-  digitalWrite(6, HIGH);
-  digitalWrite(4, HIGH);
-  digitalWrite(3, HIGH);
-  delay(5);
-  
-  RTC.configure(11,12,13,10); // for OSBSS datalogger 
-  //RTC.configure(10,11,12,13); // for bare RTC on Arduino Pro Mini or Uno
-  
-  RTC.setDateTime(DD,MM,YY,hh,mm,ss); // Format: DD/MM/YY hh:mm:ss
+  Serial.begin(115200);
+
+  // Following line sets the RTC to the date & time this sketch was compiled:
+  setupRTC(); 
 }
  
 void loop()
 {
-  Serial.println(RTC.readDateTime());
+  RtcDateTime time = RTC.GetDateTime();
+
+  int year,month,day,hour,minute,second;
+  year=time.Year(); month=time.Month(); day=time.Day(); hour=time.Hour(); minute=time.Minute(); second=time.Second();
+
+  Serial.println();
   delay(1000);
+}
+
+void setupRTC(){
+  RTC.Begin();
+
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+
+  if (!RTC.IsDateTimeValid()) {
+      // Serial.println("RTC lost confidence in the DateTime!");
+      RTC.SetDateTime(compiled);
+  }
+
+  if (!RTC.GetIsRunning()) {
+      // Serial.println("RTC was not actively running, starting now");
+      RTC.SetIsRunning(true);
+  }
+  
+  RtcDateTime now = RTC.GetDateTime();
+  if (now < compiled) {
+      // Serial.println("RTC is older than compile time!  (Updating DateTime)");
+      RTC.SetDateTime(compiled);
+  }
+  
+  RTC.Enable32kHzPin(false);
+  RTC.SetSquareWavePin(DS3234SquareWavePin_ModeAlarmBoth); 
 }
--- Software/datalog/datalog.ino
+++ Software/datalog/datalog.ino
@@ -1,175 +1,297 @@
-
-
 //****************************************************************
 // Data logging software from Leo, Lareo et al. 2018
-// Based on OSBSS T/RH datalogger code - v0.03
 //****************************************************************
 
 #include <EEPROM.h>
-#include <DS3234lib3.h>
-#include <PowerSaver.h>
-#include <Sensirion.h>
+#include <RtcDS3234.h>
+#include <Adafruit_SleepyDog.h>
+#include <SoftwareWire.h>
+#include <SHT85.h>
 #include <SdFat.h>
+#include <sdios.h>
+#include <SPI.h>
+
+
+#define DEBUG_RTC
+#define DEBUG_SENSORS
+#define DEBUG_SD 0
 
 // Constants
 #define POWA 4    // pin 4 supplies power to microSD card breakout and SHT15 sensor
 #define NREADS 5
 #define ANALOGMAX 8
-#define PINON 7
+#define RtcSquareWavePin 8 // Arduino Pro Mini
+#define INTERVAL 10
+#define maxSDLines2Serial 5
+#define line_buffer_size 300
+#define SHT85_ADDRESS 0x44
 
 // Launch Variables   ******************************
-long interval = I;  // set logging interval in SECONDS, eg: set 300 seconds for an interval of 5 mins
-int dayStart = D, hourStart = H, minStart = m;    // define logger start time: day of the month, hour, minute
-char filename[15] = "log.csv";    // Set filename Format: "12345678.123". Cannot be more than 8 characters in length, contain spaces or begin with a number
+// long interval = 10;  // set logging interval in SECONDS, eg: set 300 seconds for an interval of 5 mins
+char filename_prefix[] = "";
+char filename_ext[] = ".csv";
+char filename[20];    // Set filename Format: "12345678.123". Cannot be more than 8 characters in length, contain spaces or begin with a number
 
 // Global objects and variables   ******************************
-int SDcsPin = 9;        // pin 9 is CS pin for MicroSD breakout
-int SHT_clockPin = 3;   // pin used for SCK on SHT15 breakout
-int SHT_dataPin = 5;    // pin used for DATA on SHT15 breakout
-float VCC = 3.3;
+const int SDcsPin = 9;        // pin 9 is CS pin for MicroSD breakout
+const uint8_t DS3234_CS_PIN = 10; // RTC SS pin
+const uint8_t SHT_clockPin = 3;   // pin used for SCK on SHT85 breakout
+const uint8_t SHT_dataPin = 5;    // pin used for DATA on SHT85 breakout
+const float VCC = 3.3;
 int RValue = 330;
+int fileLastHour=-1;
+int nSDLines2Serial=0;
 
-PowerSaver chip;  	// declare object for PowerSaver class
-DS3234 RTC;         // declaTemp(C)re object for DS3234 class
-Sensirion sensor(SHT_dataPin, SHT_clockPin);  // declare object for SHT15 class
+RtcDateTime time;
+
+RtcDS3234<SPIClass> RTC(SPI, DS3234_CS_PIN);
+SoftwareWire myWire(SHT_dataPin, SHT_clockPin);
+SHT85 sensor;  // declare object for SHT85 class
 SdFat sd; 		    // declare object for SdFat class
-SdFile file;		// declare object for SdFile class
-
-// ISR ****************************************************************
-ISR(PCINT0_vect)  // Interrupt Vector Routine to be executed when pin 8 receives an interrupt.
-{
-  //PORTB ^= (1<<PORTB1);
-  asm("nop");
-}
 
 // setup ****************************************************************
 void setup()
 {
-  Serial.begin(19200); // open serial at 19200 bps
-
+  Serial.begin(115200); // open serial at 19200 bps
+  while (!Serial);
+  
   pinMode(POWA, OUTPUT);  // set output pins
+  // set the interupt pin to input mode
+  pinMode(RtcSquareWavePin, INPUT_PULLUP); // external pullup maybe required still
+  
+  SPI.begin();
 
-  digitalWrite(POWA, HIGH);    // turn on SD card
-  delay(1);    // give some delay to ensure RTC and SD are initialized properly
+  setupRTC();
+  setupSD();
+  
+  time = RTC.GetDateTime();
 
-  if (!sd.init(SPI_FULL_SPEED, SDcsPin)) // initialize SD card on the SPI bus - very important
-  {
-    delay(10);
-  }
-  else
-  {
-    delay(10);
-    file.open(filename, O_CREAT | O_APPEND | O_WRITE);  // open file in write mode and append data to the end of file
-    delay(1);
-    file.println();
-    file.print("Date/Time,Temp(C),RH(%),Dew Point(C), Impedance(ohm)");    // Print header to file
-    file.println();
-    file.close();    // close file - very important
-  }
+  myWire.begin();
+  sensor.begin(SHT85_ADDRESS,&myWire);
+  myWire.setClock(100000UL);
 
-  RTC.checkInterval(hourStart, minStart, interval); // Check if the logging interval is in secs, mins or hours
-  RTC.alarm2set(dayStart, hourStart, minStart);  // Configure begin time
-  RTC.alarmFlagClear();  // clear alarm flag
-
-  chip.sleepInterruptSetup();    // setup sleep function & pin change interrupts on the ATmega328p. Power-down mode is used here
+  delay(1);
 }
 
 // loop ****************************************************************
 void loop()
-{
+{  
   digitalWrite(POWA, LOW);  // turn off microSD card to save power
-  digitalWrite(PINON, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   delay(1);  // give some delay for SD card and RTC to be low before processor sleeps to avoid it being stuck
 
-  chip.turnOffADC();    // turn off ADC to save power
-  chip.turnOffSPI();  // turn off SPI bus to save power
-  //chip.turnOffWDT();  // turn off WatchDog Timer to save power (does not work for Pro Mini - only works for Uno)
-  chip.turnOffBOD();    // turn off Brown-out detection to save power
+  //chip.turnOffADC();    // turn off ADC to save power
+  //chip.turnOffSPI();  // turn off SPI bus to save power
+  //chip.turnOffBOD();    // turn off Brown-out detection to save power
+  
+  time = time+INTERVAL;
+  setNextAlarm(time);      //set next alarm before sleeping
 
-  chip.goodNight();    // put processor in extreme power down mode - GOODNIGHT!
+  Watchdog.sleep();    // put processor in extreme power down mode - GOODNIGHT!
   // this function saves previous states of analog pins and sets them to LOW INPUTS
   // average current draw on Mini Pro should now be around 0.195 mA (with both onboard LEDs taken out)
   // Processor will only wake up with an interrupt generated from the RTC, which occurs every logging interval
 
   // code will resume from here once the processor wakes up =============== //
-  chip.turnOnADC();    // enable ADC after processor wakes up
-  chip.turnOnSPI();   // turn on SPI bus once the processor wakes up
-  delay(1);    // important delay to ensure SPI bus is properly activated
+  //chip.turnOnADC();    // enable ADC after processor wakes up
+  //chip.turnOnSPI();   // turn on SPI bus once the processor wakes up
+  //delay(1);    // important delay to ensure SPI bus is properly activated
+  int year,month,day,hour,minute,second;
+  year=time.Year(); month=time.Month(); day=time.Day(); hour=time.Hour(); minute=time.Minute(); second=time.Second();
 
-  RTC.alarmFlagClear();    // clear alarm flag
+  #ifdef DEBUG_RTC
+    Serial.print("Awake at ");
+    SerialDebugRTC(year, month, day, hour, minute, second);
+  #endif
+  
+  RTC.LatchAlarmsTriggeredFlags();    // clear alarm flag
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+
   pinMode(POWA, OUTPUT);
-  digitalWrite(POWA, HIGH);  // turn on SD card power
-  delay(1);
-  digitalWrite(PINON, HIGH);
-  delay(1);    // give delay to let the SD card and SHT15 get full powa
+  digitalWrite(POWA, HIGH);    // turn on SD card
+  delay(1);    // give some delay to ensure RTC and SD are initialized properly
+  
+  pinMode(SDcsPin, OUTPUT);
+  if (!sd.begin(SdSpiConfig(SDcsPin))){   // very important - reinitialize SD card on the SPI bus
+    if (DEBUG_SD) Serial.println("SD ERROR");
+    sd.initErrorHalt();
+  }
+  
+  float temperature;
+  float humidity; 
+  float dewPoint;
+  float RData[8];
 
-  RTC.checkDST(); // check and account for Daylight Savings Time in US
+  getDataFromSensors(&temperature,&humidity,&dewPoint, RData);
+  #ifdef DEBUG_SENSORS
+    SerialDebugSensors(&temperature,&humidity,&dewPoint, RData);
+  #endif
 
+  // if (hour==0 && fileLastHour==23){
+  //   fileLastHour=-1;
+  // }
+  // if (hour>fileLastHour){
+  //   delay(1);
+    
+  //   fileLastHour = hour;
+
+  //   doFilename(month,day,fileLastHour);
+  //   initFile();
+  //   delay(1);
+  // }
+  // printDataEntry2File(year, month, day, hour, minute, second,&temperature,&humidity,&dewPoint,RData);
+  // if (nSDLines2Serial<maxSDLines2Serial){
+  //   nSDLines2Serial++;
+  //   Serial.println("");
+  //   Serial.print("[");
+  //   Serial.print(nSDLines2Serial);
+  //   Serial.print("] ");
+  //   lastSDLine2Serial();
+  // }
+  
+
+//  SPCR = 0;  // reset SPI control register
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+// ------------------------------------------------
+//  FUNCTIONS
+// ------------------------------------------------
+
+void setupRTC(){
+  RTC.Begin();
+
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+
+  if (!RTC.IsDateTimeValid()) {
+      // Serial.println("RTC lost confidence in the DateTime!");
+      RTC.SetDateTime(compiled);
+  }
+
+  if (!RTC.GetIsRunning()) {
+      // Serial.println("RTC was not actively running, starting now");
+      RTC.SetIsRunning(true);
+  }
+  
+  RtcDateTime now = RTC.GetDateTime();
+  if (now < compiled) {
+      // Serial.println("RTC is older than compile time!  (Updating DateTime)");
+      RTC.SetDateTime(compiled);
+  }
+  
+  RTC.Enable32kHzPin(false);
+  RTC.SetSquareWavePin(DS3234SquareWavePin_ModeAlarmBoth); 
+}
+
+void setupSD(){
+  int ret;
+  pinMode(POWA, OUTPUT);
+  digitalWrite(POWA, HIGH);    // turn on SD card
+  delay(1);    // give some delay to ensure RTC and SD are initialized properly
+
+  if (!sd.begin(SdSpiConfig(SDcsPin))){ // initialize SD card on the SPI bus - very important
+    if (DEBUG_SD) {
+      Serial.println("SD ERROR");
+      sd.initErrorHalt();
+    }
+  }
+}
+
+void initFile(){
+  ofstream file(filename,ios_base::app);
+  if (!file.is_open()) {
+    if (DEBUG_SD){
+      Serial.print("SDFile ERR: ");
+      Serial.println(filename);
+    }
+    sd.errorHalt("open");
+  }
+  file << "Date/Time,Temp(C),RH(%),Dew Point(C), Impedance(ohm)\n";    // Print header to file
+  file.close();    // close file - very important
+}
+
+void getDataFromSensors(float* temperature, float* humidity, float* dewPoint, float* RData){
   for (int a = A0; a < A0 + ANALOGMAX; a++)
     for (int i = 0; i < NREADS; i++)
       analogRead(a);  // first few readings from ADC may not be accurate, so they're cleared out here
   delay(1);
 
-  // Medida Musgos
-  float RMoss[8];
+  // Read moss data
   for (int a = 0; a < ANALOGMAX; a++){
-    float adcMoss = averageADC(A0+a);
-    RMoss[a] = mossImpedance(adcMoss, RValue);	//Reference resistor value
+    float adcData = averageADC(A0+a);
+    RData[a] = mossImpedance(adcData, RValue); //Reference resistor value
   }
 
-
-  float temperature;
-  float humidity; 
-  float dewPoint; 
-  sensor.measure(&temperature,&humidity,&dewPoint);
-  
-  pinMode(SDcsPin, OUTPUT);
-  if (!sd.init(SPI_FULL_SPEED, SDcsPin))   // very important - reinitialize SD card on the SPI bus
-  {
-    Serial.println("ERROR");
-  }
-  else
-  {
-    delay(10);
-    file.open(filename, O_WRITE | O_AT_END);  // open file in write mode
-    delay(1);
-
-    String time = RTC.timeStamp();    // get date and time from RTC
-    SPCR = 0;  // reset SPI control register
-    for (int a = 0; a < ANALOGMAX; a++){
-      Serial.print(RMoss[a]);
-      Serial.print(" ");
-    }
-    Serial.println();
-    printDataEntry(&time,&temperature,&humidity,&dewPoint,RMoss);
-
-    file.close();    // close file - very important
-  }
-  RTC.setNextAlarm();      //set next alarm before sleeping
-  delay(1);
+  // Read sensor data
+  sensor.read(); 
+  *temperature += sensor.getTemperature();
+  *humidity += sensor.getHumidity();
+  *dewPoint = 0;
 }
 
-void printDataEntry(String* time, float* temperature, float* humidity, float* dewPoint, float* RData){
-  file.print(*time);
-  file.print(",");
-  file.print(*temperature);  // print temperature upto 3 decimal places
-  file.print(",");
-  file.print(*humidity);  // print humidity upto 3 decimal places
-  file.print(",");
-  file.print(*dewPoint);
-  for (int a = 0; a < ANALOGMAX; a++){
-    file.print(",");
-    file.print(RData[a]);
+void setNextAlarm(RtcDateTime next){
+  DS3234AlarmOne alarm1(
+            next.Day(),
+            next.Hour(),
+            next.Minute(), 
+            next.Second(),
+            DS3234AlarmOneControl_HoursMinutesSecondsMatch);
+  RTC.SetAlarmOne(alarm1);
+  RTC.LatchAlarmsTriggeredFlags();
+}
+
+void lastSDLine2Serial(){
+  Serial.print("buff: ");
+  char buffer[line_buffer_size];
+
+  fstream file(filename);
+  
+  bool keepLooping = true;
+  file.seekg(-3,ios_base::end);
+
+  while(keepLooping) {
+    char ch;
+    file.get(ch);                            // Get current byte's data
+    
+    if((int)file.tellg() <= 1) {             // If the data was at or before the 0th byte
+        file.seekg(0);                       // The first line is the last line
+        keepLooping = false;                // So stop there
+    }
+    else if(ch == '\n') {                   // If the data was a newline
+        keepLooping = false;                // Stop at the current position.
+    }
+    else {                                  // If the data was neither a newline nor at the 0 byte
+        file.seekg(-2,ios_base::cur);        // Move to the front of that data, then to the front of the data before it
+    }
   }
-  file.println();
+           
+  file.getline(buffer, line_buffer_size, '\n');                      // Read the current line
+  file.close();
+  
+  Serial.println(buffer);     // Display it
+  delay(2);
+
+}
+
+void printDataEntry2File(int year, int month, int day, int hour, int minute, int second, float* temperature, float* humidity, float* dewPoint, float* RData){
+  ofstream file(filename,ios_base::app);
+
+  file << year << "-" << month << "-" << day << " "
+       << hour << ":" << minute << ":" << second << ",";
+  file << *temperature << "," << *humidity << "," << *dewPoint;
+  for (int a = 0; a < ANALOGMAX; a++){
+    file << ",";
+    file << RData[a];
+  }
+  file << "\n";
+  file.close();
 }
 
 // Averaging ADC values to counter noise in readings  *********************************************
-float averageADC(int pin)
-{
+float averageADC(int pin){
   float sum = 0.0;
-  for (int i = 0; i < NREADS; i++)
-  {
+  for (int i = 0; i < NREADS; i++){
     sum = sum + analogRead(pin);
   }
   float average = sum / 5.0;
@@ -177,25 +299,97 @@ float averageADC(int pin)
 }
 
 // Get mossImp ****************************************************************
-float mossImpedance(float adc, int R)
-{
+float mossImpedance(float adc, int R){
   float V = (adc * VCC) / 1023.0;
   return (VCC * R) / V - R;
 }
 
-// file timestamps ****************************************************************
-void printFileTimeStamp() // Print timestamps to data file. Format: year, month, day, hour, min, sec
-{
-  file.timestamp(T_WRITE, RTC.year, RTC.month, RTC.day, RTC.hour, RTC.minute, RTC.second);    // edit date modified
-  file.timestamp(T_ACCESS, RTC.year, RTC.month, RTC.day, RTC.hour, RTC.minute, RTC.second);    // edit date accessed
-}
-
-// Read file name ****************************************************************
-void readFileName()  // get the file name stored in EEPROM (set by GUI)
-{
-  for (int i = 0; i < 12; i++)
-  {
-    filename[i] = EEPROM.read(0x06 + i);
+void doFilename(int month, int day, int hour){  
+  char c[3];
+  int d;
+  strcpy(filename, filename_prefix);
+  
+  if (month >= 10){
+    c[1]= month%10 + '0';
+    month/=10;
+    c[0]=month+'0';
+    c[2]='\0';
+    strcat(filename, c);
+  }
+  else{
+    c[0]=month+'0';
+    c[1]='\0';
+    strcat(filename, c);
+  }
+  
+  c[0]='_'; c[1]='\0';
+  strcat(filename, c);
+  
+  if (day >= 10){
+    c[1] = day%10 + '0';
+    day/=10;
+    c[0]=day+'0';
+    c[2]='\0';
+    strcat(filename, c);
+  }
+  else{
+    c[0]=day+'0';
+    c[1]='\0';
+    strcat(filename, c);
+  }
+  
+  c[0]='_'; c[1]='\0';
+  strcat(filename, c);
+  
+  if (hour >= 10){
+    c[1] = hour%10 + '0';
+    hour/=10;
+    c[0]=hour+'0';
+    c[2]='\0';
+    strcat(filename, c);
+  }
+  else{
+    c[0]=hour+'0';
+    c[1]='\0';
+    strcat(filename, c);
+  }
+  
+  strcat(filename, filename_ext);
+  
+  if (DEBUG_SD){
+    Serial.print("filename: ");
+    Serial.println(filename);
+    delay(1);
   }
 }
-//****************************************************************
+
+// SERIAL DEBUG
+#ifdef DEBUG_RTC
+void SerialDebugRTC(int year, int month, int day, int hour, int minute, int second){
+  month = time.Month();
+  Serial.print(year);
+  Serial.print("-");
+  Serial.print(month);
+  Serial.print("-");
+  Serial.print(day);
+  Serial.print(" ");
+  
+  Serial.print(hour);
+  Serial.print(":");
+  Serial.print(minute);
+  Serial.print(":");
+  Serial.println(second); 
+}
+#endif 
+
+#ifdef DEBUG_SENSORS
+void SerialDebugSensors(float* temperature, float* humidity, float* dewPoint, float* RData){
+  Serial.print(*temperature);
+  Serial.print(" ");
+  Serial.print(*humidity);
+  Serial.print(" ");
+  Serial.print(*dewPoint);
+  Serial.print(" ");
+  Serial.println(RData[0]);
+}
+#endif
